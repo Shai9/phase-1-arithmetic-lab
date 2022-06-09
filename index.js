1ac159a965aeb312e6f03ad8e6f27d3425655bcc
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-09 23:17:21 +0300

    commit tree 3b5ec9d0007c5c3530d7afbd862431ba8b639d8a

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1= 1
+const num2= 62
+ const multiply= num1*num2
+ const random = Math.floor(Math.random() * 10) + 1
+ const num3 = 6
+ const num4 = 10
+ const mod =num4%num3
+const max = Math.max(10,15,20)
+
+   
